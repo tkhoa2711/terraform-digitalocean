08b7631f4e96c284c385ce3a799076b1ea6f2c1b
Khoa Le <ltkhoa2711@gmail.com>
2020-04-13 14:51:21 +1000

    add default values for SSH keys

--- variable.tf
+++ variable.tf
@@ -1,4 +1,5 @@
 variable "do_token" {
+  description = "Personal access token for DigitalOcean"
 }
 
 variable "do_ssh_key_name" {
@@ -12,7 +13,10 @@ variable "username" {
 
 variable "user_public_key" {
   description = "The public key of the user to login"
+  default     = "~/.ssh/id_rsa.pub"
 }
 
 variable "private_key" {
+  description = "The SSH private key to connect to the droplet"
+  default     = "~/.ssh/id_rsa"
 }
